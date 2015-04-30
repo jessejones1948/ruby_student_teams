bc1a0df7d4f9cb5fc1f075ea78c6c46b7d0eda66
jessejones1948 <olathanbake1@yahoo.com>
2015-04-30 08:59:40 -0400

    paired up students

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,2 +1,14 @@
 # student_shuffle.rb
 
+students = ["Brandon", "Stefanie", "Enoch", "Mandy", "Jesse", "Ernest", "Jamel", "Sabeel", "Mike"]
+
+students.shuffle!
+
+count = 0
+
+num_students = students.length - 1
+
+until count == num_students
+	puts "#{students[count]} & #{students[count+1]}"
+	count += 1
+end
